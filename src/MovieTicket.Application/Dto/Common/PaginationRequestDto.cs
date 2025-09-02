0bce8b88084a3cda62fadac9c52b58f2e94bf431
fm39hz <hitpoint2k3@gmail.com>
2025-09-02 17:26:35 +0700

    feat(pagination): add default values and validation to pagination DTO

--- src/MovieTicket.Application/Dto/Common/PaginationRequestDto.cs
+++ src/MovieTicket.Application/Dto/Common/PaginationRequestDto.cs
@@ -1,7 +1,11 @@
 namespace MovieTicket.Application.Dto.Common;
 
+using System.ComponentModel;
+
 public class PaginationRequestDto {
+	[DefaultValue(1)]
 	public int Page { get; set; } = 1;
+	[DefaultValue(10)]
 	public int PageSize { get; set; } = 10;
 	public string? SortBy { get; set; }
 	public bool SortDescending { get; set; }
@@ -9,8 +13,16 @@ public class PaginationRequestDto {
 	public int Skip => (Page - 1) * PageSize;
 
 	public void Validate() {
-		if (Page < 1) Page = 1;
-		if (PageSize < 1) PageSize = 10;
-		if (PageSize > 100) PageSize = 100;
+		if (Page < 1) {
+			Page = 1;
+		}
+
+		if (PageSize < 1) {
+			PageSize = 10;
+		}
+
+		if (PageSize > 100) {
+			PageSize = 100;
+		}
 	}
 }
